0ee5a68cba94e6451192f8f337c819788e4cdc5a
Navneet <navneet.kumar@kvpcorp.com>
2018-01-04 08:34:10 +0530

    deployment to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180103053905) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "order1s", force: :cascade do |t|
     t.date     "date_of_delivery"
     t.date     "est_date_of_delivery"
@@ -19,7 +22,7 @@ ActiveRecord::Schema.define(version: 20180103053905) do
     t.integer  "product_id"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
-    t.index ["product_id"], name: "index_order1s_on_product_id"
+    t.index ["product_id"], name: "index_order1s_on_product_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
@@ -43,8 +46,9 @@ ActiveRecord::Schema.define(version: 20180103053905) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "order1s", "products"
 end
